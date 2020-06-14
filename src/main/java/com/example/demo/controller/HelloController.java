c7641a7d33434fe4b45d2dcb893a797a9ae5e625
ihsuan30 <q7893451@gmail.com>
2020-06-14 22:38:28 +0800

    test0614

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -9,11 +9,24 @@ import java.util.Map;
 @RestController
 
 public class HelloController {
-    @GetMapping(value = "/hello")
+    @GetMapping(value = "/click/1")
     public Map<String, Object> hello() {
+        int random=(int) (Math.random() * 100);
         Map<String, Object> map = new HashMap<>();
-        map.put("hello", "Hello world!");
+        map.put("values",random);
+        map.put("dir", "1");
         return map;
 
     }
+    @GetMapping(value ="/click/2")
+    public Map<String, Object> click() {
+        int random=(int) (Math.random() * 100);
+        Map<String, Object> map = new HashMap<>();
+        map.put("dir", "2");
+        map.put("values",random);
+
+        return map;
+
+    }
+
 }
