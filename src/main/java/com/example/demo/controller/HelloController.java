904189d03b3081140af9e9ccf99d24745dc8652c
ihsuan30 <q7893451@gmail.com>
2020-06-15 16:31:00 +0800

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,31 +1,36 @@
 package com.example.demo.controller;
 
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
 
 public class HelloController {
     @GetMapping(value = "/click/1")
-    public Map<String, Object> hello() {
+    public LinkedHashMap<String, Object> hello() {
 
-        Map<String, Object> map = new HashMap<>();
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
 
         int random=(int) (Math.random() * 100);
         map.put("dir", "1");
         map.put("values",random);
+
+        System.out.println(map);
 
 
         return map;
 
     }
     @GetMapping(value ="/click/2")
-    public Map<String, Object> click() {
+    public LinkedHashMap<String, Object> click() {
 
-        Map<String, Object> map = new HashMap<>();
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
 
         int random=(int) (Math.random() * 100);
         map.put("dir", "2");
